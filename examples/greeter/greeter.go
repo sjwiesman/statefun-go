aa9eb492020dbface6e9faab244a2911946ef69f
Seth Wiesman <sjwiesman@gmail.com>
2020-08-20 20:57:36 -0500

    end to end example

---
+++ examples/greeter/greeter.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/ptypes/any"
+	"net/http"
+
+	"github.com/sjwiesman/statefun-go/pkg/flink/statefun"
+	"github.com/sjwiesman/statefun-go/pkg/flink/statefun/io"
+)
+
+var egressId = io.EgressIdentifier{
+	EgressNamespace: "example",
+	EgressType:      "greets",
+}
+
+type Greeter struct{}
+
+func (greeter Greeter) Invoke(runtime statefun.StatefulFunctionRuntime, _ *any.Any) error {
+	var seen SeenCount
+	if err := runtime.Get("seen_count", &seen); err != nil {
+		return err
+	}
+
+	seen.Seen += 1
+
+	if err := runtime.Set("seen_count", &seen); err != nil {
+		return err
+	}
+
+	response := computeGreeting(runtime.Self().Id, seen.Seen)
+
+	record := io.KafkaRecord{
+		Topic: "greetings",
+		Key:   runtime.Self().Id,
+		Value: response,
+	}
+
+	message, err := record.ToMessage()
+	if err != nil {
+		return nil
+	}
+
+	return runtime.SendEgress(egressId, message)
+}
+
+func computeGreeting(id string, seen int64) *GreetResponse {
+	templates := []string{
+		"",
+		"Welcome %s",
+		"Nice to see you again %s",
+		"Third time is a charm %s"}
+
+	greeting := &GreetResponse{}
+	if int(seen) < len(templates) {
+		greeting.Greeting = fmt.Sprintf(templates[seen], id)
+	} else {
+		greeting.Greeting = fmt.Sprintf("Nice to see you at the %d-nth time %s!", seen, id)
+	}
+
+	return greeting
+}
+
+func main() {
+	registry := statefun.NewFunctionRegistry()
+	registry.RegisterFunction(statefun.FunctionType{
+		Namespace: "example",
+		Type:      "greeter",
+	}, Greeter{})
+
+	http.Handle("/statefun", registry)
+	_ = http.ListenAndServe(":8000", nil)
+}
